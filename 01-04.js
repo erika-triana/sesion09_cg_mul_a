550705e65a5bc558e4a09a57aca4f1881fb7633a
erika-triana <est.erika.triana@unimilitar.edu.co>
2021-09-27 21:52:27 -0500

    Signed-off-by: erika-triana <est.erika.triana@unimilitar.edu.co>

--- 01-04.js
+++ 01-04.js
@@ -1,4 +1,3 @@
-// create a scene, that will hold all our elements such as objects, cameras and lights. 
 var scene = new THREE.Scene();
 
 function cubo(x, y, z, color, material, alambrado){
@@ -26,15 +25,12 @@ function cubo(x, y, z, color, material, alambrado){
     
     var cube = new THREE.Mesh(cubeGeometry, cubeMaterial)
 
-    // add the cube to the scene
     scene.add(cube);
     return(cube);
 }
 function init()
  {
-    // create a scene, that will hold all our elements such as objects, cameras and lights.
 
-    // create a camera, which defines where we're looking at.
     var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
 
     // create a render and set the size
@@ -46,7 +42,7 @@ function init()
     var axes = new THREE.AxesHelper(20);
     scene.add(axes);
 
-    Cubo = [];   // Definir un array unidimensional
+    Cubo = []; 
     Cubo.push(cubo(4, 4, 4, 0xFF0000, 'Lambert', false));
     Cubo.push(cubo(4, 4, 4, 0x00FFFF, 'Physical', false));
     Cubo.push(cubo(4, 4, 4, 0xFFFFFF, 'Basic', false));
@@ -59,18 +55,17 @@ function init()
     Cubo[3].position.set(0, 14, 0);
     Cubo[4].position.set(0, 18, 0);
 
-    //Luz (requerida para el material MeshLambertMaterial)
-    light = new THREE.PointLight(0xFFFF00); //  Luz proveniente de un punto en el espacio, 
-    light.position.set( -10, 15, 15 );             //  Localización de la luz. (x, y, z).
+    light = new THREE.PointLight(0xFFFF00);  
+    light.position.set( -10, 15, 15 );            
     scene.add( light );
 
-    // position and point the camera to the center of the scene
+    
     camera.position.set(-35, 20, 45);
     camera.lookAt(scene.position);
 
-    // add the output of the renderer to the html element
+    
     document.getElementById("webgl-output").appendChild(renderer.domElement);
 
-    // render the scene
+   
     renderer.render(scene, camera);
 }
